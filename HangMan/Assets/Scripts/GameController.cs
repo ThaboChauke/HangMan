da128dc3fdb1440af7b17c81f5235a888660d439
Thabo Chauke <chaukethabo16@gmail.com>
2024-11-05 00:44:18 +0200

    word checking and displaying

--- HangMan/Assets/Scripts/GameController.cs
+++ HangMan/Assets/Scripts/GameController.cs
@@ -15,7 +15,23 @@ public class GameController : MonoBehaviour
     void Start()
     {
         chosenWord = wordsLocal[Random.Range(0, wordsLocal.Length)];
-        wordToFind.text = chosenWord;
+
+        for (int i = 0; i < chosenWord.Length; i++)
+        {
+            char letter = chosenWord[i];
+            if (char.IsWhiteSpace(letter))
+            {
+                hiddenWord += " ";
+            }
+            else
+            {
+                hiddenWord += "_";
+            }
+        }
+
+
+
+        wordToFind.text = hiddenWord;
     }
 
     // Update is called once per frame
@@ -23,5 +39,36 @@ public class GameController : MonoBehaviour
     {
         time += Time.deltaTime;
         timeField.text = time.ToString();
+    }
+
+    private void OnGUI()
+    {
+        Event e = Event.current;
+
+        if (e.type == EventType.KeyDown && e.keyCode.ToString().Length == 1)
+        {
+            string pressedLetter = e.keyCode.ToString();
+
+            if (chosenWord.Contains(pressedLetter))
+            {
+                int i = chosenWord.IndexOf(pressedLetter);
+
+                while (i != -1)
+                {
+                    hiddenWord = hiddenWord.Substring(0,i) + pressedLetter + hiddenWord.Substring(i + 1);
+
+                    chosenWord = chosenWord.Substring(0, i) + "_" + chosenWord.Substring(i+1);
+
+                    i = chosenWord.IndexOf(pressedLetter);
+
+                }
+
+                wordToFind.text = hiddenWord;
+            }
+            else
+            {
+
+            }
+        }
     }
 }
