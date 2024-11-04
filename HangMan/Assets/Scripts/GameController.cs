9b4323a1d2f75cc2fed8ecaea79aa870f8fd7bf2
Thabo Chauke <chaukethabo16@gmail.com>
2024-11-05 00:05:17 +0200

    added random word

--- HangMan/Assets/Scripts/GameController.cs
+++ HangMan/Assets/Scripts/GameController.cs
@@ -5,12 +5,17 @@ using TMPro;
 public class GameController : MonoBehaviour
 {
     public TMP_Text timeField;
+    public TMP_Text wordToFind;
     private float time;
+    private string[] wordsLocal = { "KING", "JOHN", "MATT", "HELEN" };
+    private string hiddenWord;
+    private string chosenWord;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        chosenWord = wordsLocal[Random.Range(0, wordsLocal.Length)];
+        wordToFind.text = chosenWord;
     }
 
     // Update is called once per frame
